4d03e07e1c456737db67bda5d001576cd05e767d
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-03-18 23:06:05 +0000

    Added Favicon, Meta Data and website content to index.html, Added color to text and background to style.css and added the function to calculate the area and perimeter of the given rectangle.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+function doMathClicked () {
+  // this function does basic math
+  document.getElementById('multiply-math').innerHTML = 'The Area is ' + (5 * 3) + 'cm²'
+	document.getElementById('add-math').innerHTML = 'The Perimeter is ' + (5 + 5 + 3 + 3) + 'cm'
+}

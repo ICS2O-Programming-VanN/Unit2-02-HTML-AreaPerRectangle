8dc14e54b3121f28dcb013f9affb002ebffce990
Van Nguyen <99274250+van-nguyen2006@users.noreply.github.com>
2022-03-23 20:38:25 +0000

    Added Comments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-
 function doMathClicked () {
-  // this function does basic math
+  // this function does basic math/calculates the perimeter and area of the given triangle
   document.getElementById('multiply-math').innerHTML = 'The Area is ' + (5 * 3) + 'cm²'
 	document.getElementById('add-math').innerHTML = 'The Perimeter is ' + (5 + 5 + 3 + 3) + 'cm'
 }
